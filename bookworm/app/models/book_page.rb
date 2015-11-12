09ad9fcbdfe4dc2fbeb67478de21f68ff1d9d796
ranael-garem <ranaelgarem781994@gmail.com>
2015-11-12 12:54:34 +0200

    added message for validates_attachment

--- bookworm/app/models/book_page.rb
+++ bookworm/app/models/book_page.rb
@@ -9,7 +9,7 @@ class BookPage < ActiveRecord::Base
   	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
   	:default_url => "/images/:style/missing.png"
 	validates_attachment :image,
-	:content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
+	:content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"], :message=> "Invalid image format" }
 
 
 	has_many :reviews, dependent: :destroy
--- bookworm/app/models/user.rb
+++ bookworm/app/models/user.rb
@@ -14,7 +14,8 @@ class User < ActiveRecord::Base
   	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" },
   	:default_url => "/images/:style/missing.png"
 	validates_attachment :avatar,
-	:content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
+	:content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"],
+	:message=> "Invalid image format" }
 
 	after_create :message => "Thanks for using Bookworm!"
 
