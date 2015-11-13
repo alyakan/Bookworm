c0193409c25bd836f2fa13e61749a0df8bdc998f
Menna <mennaelkashef23@gmail.com>
2015-11-13 23:22:23 +0200

    modified the content of the status update posts

--- bookworm/app/models/user_read_book.rb
+++ bookworm/app/models/user_read_book.rb
@@ -12,11 +12,23 @@ class UserReadBook < ActiveRecord::Base
 	after_create :create_post
 	after_update :create_post
 		def create_post
+			user = User.find(self.user_id)
+			@user_name = user.first_name + ' ' + user.last_name
+			book = BookPage.find(self.book_page_id)
+			@book_title = book.title
 			p = Post.new
 			p.user1_id = self.user_id
 			p.user2_id = self.user_id
 			p.book_page_id = self.book_page_id
-			p.content = 'status upated'
+			if status==0
+				p.content = @user_name + ' added ' + @book_title + ' to his/her reading list'
+			end
+			if status==1
+				p.content = @user_name + ' is currently reading ' + @book_title
+			end
+			if status==2 
+				p.content = @user_name + ' has read ' + @book_title
+			end
 			p.save
 		end
 end
